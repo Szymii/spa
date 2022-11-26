bd6d8fbc0a7ee18177802c6ca396858664cc0622
Szymii <szymimelzer9@gmail.com>
2022-11-26 19:10:50 +0100

    css fixes

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,11 +2,13 @@
   <ul class="list">
     <li v-for="book in props.list" :key="book.id">
       <RouterLink :to="book.id" class="link listItem">
-        <img :src="book.cover" class="cover" />
+        <div class="coverWrapper">
+          <img :src="book.cover" class="cover" />
+        </div>
 
         <div class="description">
           <span>{{ book.name }}</span>
-          <span class="price">{{ book.price }} zł</span>
+          <span class="author">{{ book.author }}</span>
         </div>
       </RouterLink>
     </li>
@@ -22,8 +24,11 @@ const props = defineProps(["list"]);
   margin: 0;
   padding: 0;
   list-style: none;
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-template-rows: auto;
+  padding-bottom: 2em;
 }
 .listItem {
   display: flex;
@@ -31,8 +36,7 @@ const props = defineProps(["list"]);
 }
 
 .cover {
-  width: 250px;
-  height: 400px;
+  max-height: 320px;
   object-fit: cover;
 }
 
@@ -42,20 +46,20 @@ const props = defineProps(["list"]);
 }
 
 .description {
-  display: flex;
-  justify-content: space-between;
   font-weight: bold;
   margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  padding-inline: calc((100% - 230px) / 2);
 }
 
-.price {
-  color: rgb(255, 123, 0);
+.coverWrapper {
+  display: flex;
+  justify-content: center;
 }
 
-@media (max-width: 900px) {
-  .cover {
-    width: 150px;
-    height: 225px;
-  }
+.author {
+  color: gray;
+  font-weight: 400;
 }
 </style>
